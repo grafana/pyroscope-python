f97593f8aff5668756fbe7c7e3f2b1ce3b75fbb0
Tolyan Korniltsev <anatoly.korniltsev@grafana.com>
2026-07-17 18:22:53 +0700

    test: keep retry checks within test lifetime

--- integration-test/require/eventually.go
+++ integration-test/require/eventually.go
@@ -8,32 +8,19 @@ import (
 func Eventually(t *testing.T, condition func() bool, waitFor time.Duration, tick time.Duration, msgAndArgs ...interface{}) bool {
 	t.Helper()
 
-	ch := make(chan bool, 1)
-	checkCond := func() { ch <- condition() }
-
-	timer := time.NewTimer(waitFor)
-	defer timer.Stop()
-
-	ticker := time.NewTicker(tick)
-	defer ticker.Stop()
-
-	var tickC <-chan time.Time
-
-	// Check the condition once first on the initial call.
-	go checkCond()
-
+	deadline := time.Now().Add(waitFor)
 	for {
-		select {
-		case <-timer.C:
-			return Fail(t, "Condition never satisfied", msgAndArgs...)
-		case <-tickC:
-			tickC = nil
-			go checkCond()
-		case v := <-ch:
-			if v {
-				return true
-			}
-			tickC = ticker.C
+		if condition() {
+			return true
 		}
+
+		remaining := time.Until(deadline)
+		if remaining <= 0 {
+			return Fail(t, "Condition never satisfied", msgAndArgs...)
+		}
+		if tick < remaining {
+			remaining = tick
+		}
+		time.Sleep(remaining)
 	}
 }
